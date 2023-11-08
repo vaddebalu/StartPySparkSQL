6b4d802d60d3cb98706d785c47d7d43793a4c56c
vaddebalu <vadde.balu@gmail.com>
2023-11-08 17:52:32 +0530

    Perform aggregate operation in PySpark

---
+++ aggregates/count.py
@@ -0,0 +1,13 @@
+"""
+PySpark program to shows how to perform count aggregate
+"""
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+empSchema=StructType([StructField('ename',StringType(),True),
+                StructField('eno',IntegerType(),True),
+                StructField('salary',IntegerType(),True),
+                StructField('dno',IntegerType(),True)])
+emp=spark.read.option('header',True).schema(empSchema).csv('data/emp/emp.csv')
+
+emp.groupby('dno').count().show()
